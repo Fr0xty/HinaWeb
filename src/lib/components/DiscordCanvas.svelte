00354ebe1ca7e0d249a5a484d1844e762f1e91a0
Fr0xty <ongjiae8@gmail.com>
2023-01-12 17:10:11 +0800

    added ScrollHint component to hero section

--- src/lib/components/DiscordCanvas.svelte
+++ src/lib/components/DiscordCanvas.svelte
@@ -1,20 +1,35 @@
 <script lang="ts">
     import DiscordCanvasContent from '$lib/components/DiscordCanvasContent.svelte';
     import DiscordUserBar from '$lib/components/DiscordUserBar.svelte';
+    import ScrollHint from './ScrollHint.svelte';
 </script>
 
 <div class="discord-canvas">
     <DiscordCanvasContent />
     <DiscordUserBar />
+
+    <div class="scroll-hint">
+        <ScrollHint />
+    </div>
 </div>
 
 <style lang="scss">
     .discord-canvas {
+        position: relative;
         background: #36393f;
         min-height: 100%;
         min-width: 100%;
         display: grid;
         grid-template-columns: 75% 25%;
+
+        .scroll-hint {
+            position: absolute;
+            left: 0;
+            right: 0;
+            bottom: 4%;
+            display: flex;
+            justify-content: center;
+        }
     }
 
     @media (max-width: 1200px) {
---
+++ src/lib/components/ScrollHint.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+    import { fade } from 'svelte/transition';
+    import { onMount } from 'svelte';
+
+    let componentMounted = false;
+    onMount(() => (componentMounted = true));
+</script>
+
+{#if componentMounted}
+    <div class="scroll-hint" in:fade={{ duration: 1000, delay: 1500 }}>
+        <div class="point" />
+    </div>
+{/if}
+
+<style lang="scss">
+    @keyframes Bounce {
+        0% {
+            top: 50%;
+            background: white;
+            opacity: 1;
+        }
+        7% {
+            top: 0%;
+        }
+        11% {
+            background: gray;
+        }
+        35% {
+            top: 50%;
+            color: gray;
+            opacity: 20%;
+        }
+        100% {
+            top: 50%;
+            color: gray;
+            opacity: 20%;
+        }
+    }
+
+    .scroll-hint {
+        height: 2.5em;
+        width: 2.5em;
+        border: 1px solid gray;
+        border-radius: 50%;
+        position: relative;
+
+        .point {
+            margin: 0.3em auto;
+            height: 0.5em;
+            width: 0.5em;
+            position: absolute;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            background: gray;
+            border-radius: 50%;
+            animation: Bounce 6s cubic-bezier(0.26, 0.62, 0.49, 0.96) 3.5s infinite;
+        }
+    }
+
+    @media (max-width: 1010px) {
+        .scroll-hint {
+            height: 2em;
+            width: 2em;
+
+            .point {
+                height: 0.4em;
+                width: 0.4em;
+            }
+        }
+    }
+</style>
