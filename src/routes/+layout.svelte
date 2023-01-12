0bd174675622c2f8cc555f216727834814e99f98
Fr0xty <ongjiae8@gmail.com>
2023-01-12 18:29:07 +0800

    added timeout for scrollbar to disappear

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,17 @@
     import '../app.scss';
     import { onMount } from 'svelte';
 
+    let isScrolling: NodeJS.Timeout;
+
     onMount(() => {
         document.addEventListener('scroll', () => {
             document.body.classList.add('scroll');
+
+            window.clearTimeout(isScrolling);
+
+            isScrolling = setTimeout(() => {
+                document.body.classList.remove('scroll');
+            }, 1000);
         });
     });
 </script>
